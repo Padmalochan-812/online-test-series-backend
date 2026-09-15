460c2655d22ba708bdbf06f7bd44dac6eada9448
Padmalochan-812 <sahoopadma812@gmail.com>
2026-09-16 01:41:44 +0530

    feat: refactor addSubject and updateSubject functions for improved error handling and response structure

--- src/controllers/subject.controllers.js
+++ src/controllers/subject.controllers.js
@@ -3,17 +3,64 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { Test } from "../models/test.model.js"
 import { Test_Series } from "../models/test_series.model.js";
-import {subject} from "../models/subject.model.js"
+import {Subject, subject} from "../models/subject.model.js"
 
 const addSubject  = asyncHandler(async(req, res) => {
     const {testId} = req.params;
     const { name } = req.body
     const test = await Test.findById(testId)
 
+    if(!name){
+        throw new apiError (404, "All fields are required!")
+    }
+
     if(!test ){
         throw new apiError (404, "test dose nor exist")
     }
-
     
+    const subject = await Subject.create(
+        {
+            name,
+            test:  testId
+        }
+    )
+    if(!subject){
+        throw new apiError(404, "subject not created!")
+    }
+    return res
+    .status(200)
+    .json(new apiResponse(200, subject, "subject added successfully."))
 })
 
+const updateSubject= asyncHandler(async(req, res) => {
+    const {name}= req.body;
+    const {testId} = req.params;
+
+    const test = await findById({
+        testId,
+        {
+            $set:{
+                title,
+                duration,
+                totalMarks,
+                totalTime,
+                startTime,
+                endTime
+            }
+        },
+        {
+            returnDocument: "after"
+        }
+    )
+
+    if(!updatedTest){
+        throw new apiError(401, "test not update ")
+    }
+
+    return res.status(200).json(
+        new apiResponse(200, updatedTest, "test series Details update successful")
+    )
+})
+
+    })
+})
